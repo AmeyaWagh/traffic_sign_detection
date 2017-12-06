16857bb8e42bd8987fae655223072f07c54f0024
AmeyaWagh <ameya555@gmail.com>
2017-12-05 23:35:48 -0500

    label box created

--- darkflow/testflow.py
+++ darkflow/testflow.py
@@ -50,8 +50,14 @@ try:
                 continue
             # print(x1,y1,x2,y2,conf,label)
             cv2.rectangle(imgcv,(x1,y1),(x2,y2),(0,255,0),6)
-            # cv2.rectangle(imgcv,(x1,y1),(x1+10,y1+2),(0,255,0),4)
-            cv2.putText(imgcv,label,(x1+10,y1+2),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,0,0),2)
+            labelSize=cv2.getTextSize(label,cv2.FONT_HERSHEY_COMPLEX,0.5,2)
+            print('labelSize>>',labelSize)
+            _x1 = x1
+            _y1 = y1#+int(labelSize[0][1]/2)
+            _x2 = _x1+labelSize[0][0]
+            _y2 = y1-int(labelSize[0][1])
+            cv2.rectangle(imgcv,(_x1,_y1),(_x2,_y2),(0,255,0),cv2.FILLED)
+            cv2.putText(imgcv,label,(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0),1)
         cv2.imshow('detected objects',imgcv)
         if cv2.waitKey(0) == ord('q'):
             break
